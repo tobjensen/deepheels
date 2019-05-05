080ee330ffa8bce718dd2f116ad574114e30d34c
Tobias Jensen <tob.jensen@gmail.com>
2019-05-05 22:28:18 +1000

    initial commit

---
+++ lambda/like.py
@@ -0,0 +1,36 @@
+import random
+import boto3
+
+table = boto3.resource('dynamodb', region_name='us-east-1').Table('shoes')
+
+def lambda_handler(event, context):
+    print(event)
+    like = str(event['like'])
+    liked = event['liked']
+    grid = event['grid']
+    
+    changes = []
+    
+    response = table.get_item( Key={'id': like} )
+    similar_items = response['Item']['items']
+    
+    open_spots = [i for i in range(len(grid)) if grid[i] not in similar_items and grid[i] not in liked]
+    print(f'Open spots: {open_spots}')
+    
+    new_items = [item for item in similar_items if item not in grid and item not in liked]
+    print(f'New items: {new_items}')
+
+    
+    swap_positions = random.sample(open_spots, min(2, len(open_spots), len(new_items)))
+    for position in swap_positions:
+        item_id = new_items.pop()
+        changes.append({'position':position, 'id':item_id})
+        print(f'Replace {grid[position]} at position {position} with {item_id}')
+        grid[position] = item_id
+
+    print('New grid:', grid)
+
+    return {
+        'statusCode': 200,
+        'changes': changes
+    }
---
+++ shoes.js
@@ -0,0 +1,56 @@
+function getGrid(select){
+	var gridSize = select.value
+	$('.shoes').empty();
+	liked = []
+
+	$.post({
+		url: 'https://xkv8t7uwu2.execute-api.us-east-1.amazonaws.com/api/',
+		data: JSON.stringify({'grid_size': gridSize}),
+		success: function ( data ) {
+			for (i = 0; i < gridSize; i++) {
+				url = 'https://s3.amazonaws.com/deepheels/' + data["grid"][i] + '.jpg'
+				$('.shoes').append(`
+					<div 
+					class = "box" 
+					style = "background-image : url( ${url} )" 
+					data-id = ${data["grid"][i]}>
+					<i></i>
+					</div>`)
+			}
+		}
+	})
+}
+
+getGrid({value: "5"})
+
+$(document).on('click', 'i', function() { 
+	var like = $(this).parent().data('id')
+	$(this).css( { "color": "#F00" } )
+
+	if (!liked.includes(like)) {
+		liked.push(like)
+	}
+
+	var grid = []
+	$('.box').each(function () {
+		grid.push($(this).data('id'))
+	})
+
+	var data = {
+		like : like,
+		liked : liked,
+		grid : grid
+	}
+
+	$.post({
+		url: 'https://2w4q7c6yaj.execute-api.us-east-1.amazonaws.com/api/',
+		data: JSON.stringify(data),
+		success: function ( data ) {
+			for (i = 0; i < data['changes'].length; i++) {
+				url = 'https://s3.amazonaws.com/deepheels/' + data['changes'][i]['id'] + '.jpg'
+				$(`.box:eq(${data['changes'][i]['position']})`).css( { "background-image": `url( ${url}` } )
+				$(`.box:eq(${data['changes'][i]['position']})`).data('id', data['changes'][i]['id'] )
+			}
+		}
+	})
+})
